0df151c337025e8f8235027d637cbc922daa6708
Arthur Reutenauer <Arthur.Reutenauer@xively.com>
2014-03-14 16:07:01 +0000

    LCD sign, messing around.

---
+++ sign.rb
@@ -0,0 +1,44 @@
+require 'socket'               # Get sockets from stdlib
+
+socket = TCPSocket.new '192.168.60.8', 3001
+
+class String
+  def ord
+    bytes.to_a.first
+  end
+end
+
+def add_header
+  [0x00, 0x00, 0x00, 0x00, 0x00,
+   0x01,
+   'Z'.ord,
+   '0'.ord, '0'.ord,
+   0x02].pack("C*")
+end
+
+def add_footer
+  [0x04].pack("C")
+end
+
+def encode_body(message)
+  s = ['A',
+   'A',
+   0x1B.chr, '0', 'a', 'this is a message'
+  ].join("")
+end
+
+def encode_message(message)
+  message = ""
+  message += add_header
+  message += encode_body(message)
+  message += add_footer
+end
+
+def send_message(client, message)
+  encoded_message = encode_message(message)
+  puts encoded_message.inspect
+  puts client.write(encoded_message).inspect
+end
+
+send_message(socket, "message here")
+socket.close
